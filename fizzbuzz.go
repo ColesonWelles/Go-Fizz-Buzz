e408b9a6b297eb9099712b9288c504fea4d30b69
ColesonWelles <cahh38@protonmail.com>
2021-06-28 13:03:58 -0700

    rewrite completed

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,4 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; 
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 {
+			fmt.Println("fizz")
+		}
+		if i%5 == 0 {
+			fmt.Println("buzz")
+		}
+		if i%3 != 0 && i%5 != 0 {
+			fmt.Println(i)
+		}
+	}
+}
